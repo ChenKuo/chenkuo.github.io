fe8acdb75d360b52505fb4a5d7b16f67682ae287
ChenKuo <ckuo1@umbc.edu>
2016-06-03 19:46:35 -0400

    o

--- main.js
+++ main.js
@@ -207,7 +207,7 @@ var ColorPanel= function(x,y,w,h,cID){
 		
 		
 		loadIdentity();
-		//mvPushMatrix();
+		mvPushMatrix();
 		mvScale([self.w,self.h,1]);
 		mvTranslate([x, y, -1.0]);
 		gl.bindBuffer(gl.ARRAY_BUFFER, self.verticesBuffer);
@@ -220,7 +220,7 @@ var ColorPanel= function(x,y,w,h,cID){
 		for(var i=0;i<len;i++){
 			gl.drawArrays(gl.TRIANGLE_STRIP, i*4, 4);
 		}
-		//mvPopMatrix();
+		mvPopMatrix();
 	};
 	
 };
